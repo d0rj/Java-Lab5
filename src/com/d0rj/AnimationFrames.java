6213eeea1032fb06ee66e3a9b6eb0b49031a6f4b
d0rj_ <dima.balobin_barnaul@mail.ru>
2020-09-04 19:44:24 +0300

    Added deer walk animation without interaction

---
+++ src/com/d0rj/AnimationFrames.java
@@ -0,0 +1,25 @@
+package com.d0rj;
+
+import javax.swing.*;
+
+public class AnimationFrames {
+    private final ImageIcon[] frames;
+    private int currentFrame = 0;
+
+    public AnimationFrames(ImageIcon[] frames) {
+        this.frames = frames;
+    }
+
+    public void reset() {
+        currentFrame = 0;
+    }
+
+    public ImageIcon getNextFrame() {
+        ImageIcon result = frames[currentFrame];
+
+        if (++currentFrame == frames.length)
+            reset();
+
+        return result;
+    }
+}
--- src/com/d0rj/windows/MyEditor.java
+++ src/com/d0rj/windows/MyEditor.java
@@ -1,33 +1,73 @@
 package com.d0rj.windows;
 
+import com.d0rj.AnimationFrames;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.net.URL;
 import javax.swing.*;
 import javax.swing.text.*;
 
 
 public class MyEditor extends JFrame implements KeyListener {
 
+    private JLabel imageLabel;
+
     private static final String MAIN_TITLE = "W&R";
     private static final String DEFAULT_FONT_FAMILY = "SansSerif";
     private static final int DEFAULT_FONT_SIZE = 14;
-    private static final int DEFAULT_ICON_SIZE = 40;
-    private static final int DEFAULT_TIMEOUT = 200;
+    private static final int DEFAULT_ICON_SIZE = 50;
+    private static final int DEFAULT_TIMEOUT = 100;
 
     private int types = 0;
+    private boolean isRunningDeer = false;
 
+    private AnimationFrames walkAnimation;
+
+
+    private URL loadResource(String path) {
+        return this.getClass().getResource("/resources/" + path);
+    }
+
+    private ImageIcon rescaleIcon(ImageIcon original) {
+        return new ImageIcon(
+                original.getImage().getScaledInstance(DEFAULT_ICON_SIZE, DEFAULT_ICON_SIZE, Image.SCALE_FAST)
+        );
+    }
+
+    private void initAnimations() {
+        walkAnimation = new AnimationFrames(
+                new ImageIcon[]{
+                        rescaleIcon(new ImageIcon(loadResource("images/deer_walk_1.png"))),
+                        rescaleIcon(new ImageIcon(loadResource("images/deer_walk_2.png"))),
+                        rescaleIcon(new ImageIcon(loadResource("images/deer_walk_3.png"))),
+                        rescaleIcon(new ImageIcon(loadResource("images/deer_walk_4.png"))),
+                        rescaleIcon(new ImageIcon(loadResource("images/deer_walk_5.png"))),
+                        rescaleIcon(new ImageIcon(loadResource("images/deer_walk_6.png"))),
+                        rescaleIcon(new ImageIcon(loadResource("images/deer_walk_7.png"))),
+                        rescaleIcon(new ImageIcon(loadResource("images/deer_walk_8.png"))),
+                        rescaleIcon(new ImageIcon(loadResource("images/deer_walk_9.png"))),
+                        rescaleIcon(new ImageIcon(loadResource("images/deer_walk_10.png"))),
+                        rescaleIcon(new ImageIcon(loadResource("images/deer_walk_11.png"))),
+                        rescaleIcon(new ImageIcon(loadResource("images/deer_walk_12.png"))),
+                }
+        );
+    }
 
     public MyEditor() {
         super(MAIN_TITLE);
 
         UIManager.put("TextPane.font", new Font(DEFAULT_FONT_FAMILY, Font.PLAIN, DEFAULT_FONT_SIZE));
 
+        initAnimations();
+
         var typesCounter = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                imageLabel.setIcon(walkAnimation.getNextFrame());
                 types = 0;
             }
         };
@@ -37,15 +77,14 @@ public class MyEditor extends JFrame implements KeyListener {
 
     public void run() {
         var editor = new JTextPane();
-        var imageLabel = new JLabel();
+        imageLabel = new JLabel();
         JScrollPane editorScrollPane = new JScrollPane(editor);
 
         editor.setDocument(new DefaultStyledDocument());
         editor.addKeyListener(this);
 
-        ImageIcon ii = new ImageIcon(this.getClass().getResource("/resources/images/deer_walk_1.png"));
-        var image = ii.getImage().getScaledInstance(DEFAULT_ICON_SIZE, DEFAULT_ICON_SIZE, Image.SCALE_FAST);
-        imageLabel.setIcon(new ImageIcon(image));
+        ImageIcon ii = new ImageIcon(loadResource("images/deer_walk_1.png"));
+        imageLabel.setIcon(rescaleIcon(ii));
 
         var toolBar = new JToolBar("Tools");
         toolBar.add(imageLabel);
